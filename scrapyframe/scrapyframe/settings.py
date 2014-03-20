b7b41f3e95e99dcd346d1973d430ca9e17c5dff1
dongshuangfei <dongshuangfei@chexiang.com>
2014-03-20 19:05:51 +0800

    Create Scrapy Project

---
+++ scrapyframe/scrapyframe/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for scrapyframe project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'scrapyframe'
+
+SPIDER_MODULES = ['scrapyframe.spiders']
+NEWSPIDER_MODULE = 'scrapyframe.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'scrapyframe (+http://www.yourdomain.com)'
